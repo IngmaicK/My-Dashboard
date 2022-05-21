c7751a1d0d1a115b6f5e5d489182e015031a7e72
Ramirez Ambrocio <ramirm15@medtronic.com>
2022-05-21 10:41:37 -0700

    Correcciones de formato

--- chart.js
+++ chart.js
@@ -17,7 +17,7 @@ const setGraph = (data) => {
     const dataConfig = {
         labels: labels,
         datasets: [{
-            label : 'my first dataSet',
+            label : 'Air Pollution Level',
             backgroundColor: 'rgb(255,99,132)',
             borderColor: 'rgb(255,99,132)',
             data: convertData,
@@ -68,7 +68,7 @@ const setGraph2 = (data) => {
     const dataConfig = {
         labels: labels,
         datasets: [{
-            label : 'my first dataSet',
+            label : 'Pollution Level Line Chart',
             backgroundColor: 'rgb(255,99,132)',
             borderColor: 'rgb(255,99,132)',
             data: convertData,
@@ -97,3 +97,53 @@ const setGraph2 = (data) => {
 
 }
 getWeather2()
+
+//Here begins 3rd chart
+// const getWeather3 = async () => {
+//     const url = 'https://api.waqi.info/feed/mexico/?token=bfa4362bd4d05d8afb40b09afe745c48b2b8b583'
+//     const res = await fetch(url)
+
+//     console.log(res)
+//     const data = await res.json()
+
+//     console.log(data)
+//     setGraph2(data.data)
+// }
+
+// const setGraph3 = (data) => {
+//     const labels = Object.keys(data.iaqi)
+//     const convertData = Object.values(data.iaqi).map(item => item.v)
+//     console.log(convertData)
+
+//     const dataConfig = {
+//         labels: labels,
+//         datasets: [{
+//             label : 'Pollution Level Line Chart',
+//             backgroundColor: 'rgb(255,99,132)',
+//             borderColor: 'rgb(255,99,132)',
+//             data: convertData,
+//         }]
+//     };
+
+
+//     const config = {
+//         type: 'line',
+//         data: dataConfig,
+//         options: {
+//             scales: {
+//              y: {
+//                  beginAtZero: true
+//                  }
+//             }
+//         }
+//     };
+    
+//     const myChart = new Chart(
+//         document.getElementById('myChart3'),
+//         config
+
+//     )
+
+
+// }
+// getWeather3()
